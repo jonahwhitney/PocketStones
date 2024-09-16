c6e9072955589615267a61a78b60ec8527710c6c
jonahwhitney11 <jwhitney@students.mchenry.edu>
2024-09-16 09:46:56 -0500

    Initial Commit

---
+++ PocketStones/PocketStonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketStonesApp.swift
+//  PocketStones
+//
+//  Created by Jonah Whitney on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct PocketStonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
